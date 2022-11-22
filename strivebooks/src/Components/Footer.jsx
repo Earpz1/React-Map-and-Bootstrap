4aca9ca6ae8f2b5c087bea44970b18fd7ab449af
Daniel <dan.earp@icloud.com>
2022-11-22 13:38:16 +0000

    Update Footer.jsx

--- strivebooks/src/Components/Footer.jsx
+++ strivebooks/src/Components/Footer.jsx
@@ -3,12 +3,12 @@ import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 
 const Footer = () => (
-  <>
+  
     <Row className="align-content-end">
       <Col className="footer dark pb-5 pt-3">
         <footer>StriveBooks - {new Date().getFullYear()}</footer>
       </Col>
     </Row>
-  </>
+
 )
 export default Footer
